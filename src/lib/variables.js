54114e207889228124de2c11d3a2f000d6a908d1
habib610 <habib54562@gmail.com>
2021-08-23 00:45:26 +0600

    initial-instagram

---
+++ src/lib/variables.js
@@ -0,0 +1,6 @@
+export const apiKey = 'AIzaSyB2PEn_2YIGYUSP99Ru7iclENiE4FLuo8A';
+export const authDomain = 'instagram-53a21.firebaseapp.com';
+export const projectId = 'instagram-53a21';
+export const storageBucket = 'instagram-53a21.appspot.com';
+export const messagingSenderId = '987962045619';
+export const appId = '1:987962045619:web:b99d490ef17e9b26846cc6';
